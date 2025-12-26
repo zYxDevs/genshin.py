afd473a2afe967a09f3866606fe124759d911953
seria <seria.ati@gmail.com>
2025-12-26 12:15:37 +0900

    feat: Support new Genshin mimo (Hilicchurl's Workshop)

--- genshin/client/components/hoyolab.py
+++ genshin/client/components/hoyolab.py
@@ -133,7 +133,7 @@ class HoyolabClient(base.BaseClient):
             raise ValueError("Cannot determine game for this traveling mimo request.")
 
         if game_id == 2 or self.game is types.Game.GENSHIN:
-            url = routes.MIMO_URL.get_url() / "nata" / endpoint.replace("-", "_")
+            url = routes.MIMO_URL.get_url() / "qiuqiu" / endpoint.replace("-", "_")
         else:
             url = routes.MIMO_URL.get_url() / endpoint
         return await self.request(url, method=method, params=params, data=data)
